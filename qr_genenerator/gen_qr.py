810021c6899a75825ebc2ece33e6711e65ca53a9
jegorix <0311egor@gmail.com>
2025-04-08 23:27:08 +0300

    FIX script for generate QR

--- qr_genenerator/gen_qr.py
+++ qr_genenerator/gen_qr.py
@@ -6,9 +6,37 @@
 pip install wifi_qrcode_generator
 '''
 
+# import wifi_qrcode_generator as qr
+#
+# qrCode = qr.wifi_qrcode('название WIFI', False, 'WPA', 'пароль')
+# qrCode.show()
+#
+# qrCode.save("qrs/wifi_qr_1.png")
+
+# import wifi_qrcode_generator as qr
+#
+# qrCode = qr.wifi_qrcode('ds 813_5G', False, 'WPA', '81301202412')
+# qrCode.show()
+#
+# qrCode.save("qrs/wifi_qr_1.png")
+
+
 import wifi_qrcode_generator as qr
+from PIL import Image
+import os
 
+# Generate the QR code object
 qrCode = qr.wifi_qrcode('название WIFI', False, 'WPA', 'пароль')
-qrCode.show()
 
-qrCode.save("qrs/wifi_qr_1.png")
+# Convert the QRCode object to an image
+qrImage = qrCode.make_image(fill_color="black", back_color="white")
+
+# Ensure the output directory exists
+if not os.path.exists("qrs"):
+    os.makedirs("qrs")
+
+# Display the QR code
+qrImage.show()
+
+# Save the QR code to a file
+qrImage.save("qrs/wifi_qr_1.png")
