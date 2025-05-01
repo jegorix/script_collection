f7d4890c27973f617008aa946dd858594d718f05
jegorix <0311egor@gmail.com>
2025-05-01 17:35:48 +0300

    update stopwatch --v2

--- algorithms&data/tkinter/stopwatch.py
+++ algorithms&data/tkinter/stopwatch.py
@@ -12,31 +12,49 @@ after_id = ''
 
 def tick():
     global temp, after_id
-    after_id = root.after(1000, tick)
-    f_temp = datetime.fromtimestamp(temp).strftime('%M:%S')
+    after_id = root.after(50, tick)
+    temp += 0.05
+
+    minutes = int(temp // 60)
+    seconds = int(temp % 60)
+    milliseconds = int((temp - int(temp)) * 100)
+
+    f_temp = f'{minutes:02}:{seconds:02}:{milliseconds:02}'
     clock_label.configure(text=str(f_temp))
-    temp += 1
 
 
 def start_tick():
+    btn_start.place_forget()
+    btn_stop.place(relx=0.5, rely=0.45, anchor='center')
     tick()
+
 
 def stop_tick():
     root.after_cancel(after_id)
+    btn_stop.place_forget()
+    btn_continue.place(relx=0.5, rely=0.45, anchor='center')
+    btn_reset.place(relx=0.5, rely=0.6, anchor='center')
+
+
 
 def continue_tick():
+    btn_continue.place_forget()
+    btn_stop.place(relx=0.5, rely=0.45, anchor='center')
+
     tick()
 
 def reset_tick():
     global temp
     temp = 0
-    clock_label.configure(text='00:00')
+    clock_label.configure(text='00:00:00')
+    btn_reset.place_forget()
+    btn_start.place(relx=0.5, rely=0.45, anchor='center')
 
 
 
 
 
-clock_label = Label(root, text='00:00', bg='black', fg='white', font=("Open Sans MS", 45, "bold"))
+clock_label = Label(root, text='00:00:00', bg='black', fg='white', font=("Open Sans MS", 45, "bold"))
 clock_label.place(relx=0.5, rely=0.15, anchor='center')
 
 btn_start = Button(root, text="Start" ,font=("Open Sans MS", 30), bg="black", fg="black", width=5, command=start_tick)
@@ -44,13 +62,12 @@ btn_start.place(relx=0.5, rely=0.45, anchor='center')
 
 
 btn_continue = Button(root, text="Continue" ,font=("Open Sans MS", 30), bg="black", fg="black", width=5, command=continue_tick)
-btn_continue.place(relx=0.5, rely=0.60, anchor='center')
 
 btn_stop = Button(root, text="Stop" ,font=("Open Sans MS", 30), bg="black", fg="black", width=5, command=stop_tick)
-btn_stop.place(relx=0.5, rely=0.75, anchor='center')
+
 
 btn_reset = Button(root, text="Reset" ,font=("Open Sans MS", 30), bg="black", fg="black", width=5, command=reset_tick)
-btn_reset.place(relx=0.5, rely=0.9, anchor='center')
+
 
 
 
