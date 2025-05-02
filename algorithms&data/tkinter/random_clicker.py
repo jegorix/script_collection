eeb6bf2d32ae307bcf1dc8fdc22d1f7780a6e7dd
jegorix <0311egor@gmail.com>
2025-05-02 16:44:52 +0300

    create pattern to involve stopwatch in random_clicker

--- algorithms&data/tkinter/random_clicker.py
+++ algorithms&data/tkinter/random_clicker.py
@@ -18,6 +18,25 @@ def change_pos():
     btn_click.place(relx=random(), rely=random(), anchor=CENTER)
 
 
+def tick_timer():
+    pass
+
+
+
+
+def start_game():
+    global clicks
+    clicks = 0
+    LabelClick.configure(text=str(clicks))
+    btn_click.place(relx=0.5, rely=0.3, anchor=CENTER)
+    btn_start.place_forget()
+    tick_timer()
+    change_pos()
+
+
+
+def end_game():
+    pass
 
 
 
@@ -26,8 +45,11 @@ LabelClick = Label(root, text='0', font=("Comis Sans MS", 40, "bold"), bg="black
 LabelClick.place(relx=0.5, rely=0.1, anchor=CENTER)
 
 btn_click = Button(root, font=("Comis Sans MS", 25, "bold"), text="Click", bg="white", fg="black", padx=15, pady=10, command = change_pos)
-btn_click.place(relx=0.5, rely=0.3, anchor=CENTER)
+btn_start = Button(root, font=("Comis Sans MS", 25, "bold"), text="Start", bg="white", fg="black", padx=15, pady=10, command = start_game)
+btn_start.place(relx=0.5, rely=0.4, anchor=CENTER)
 
+timer_label = Label(root, text="00:00:00", font=("Comic Sans MS", 30, "bold"), bg="black", fg="white")
+timer_label.place(relx=0.07, rely=0.07, anchor=CENTER)
 
 
 root.mainloop()
