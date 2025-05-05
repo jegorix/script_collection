80daa28a5e2e1ce44fa7d75812d6d91ade665bf7
jegorix <0311egor@gmail.com>
2025-05-05 18:37:02 +0300

    session and authorization, parse secure text

---
+++ algorithms&data/study_parsing/ru-board.py
@@ -0,0 +1,61 @@
+import requests
+from bs4 import BeautifulSoup
+import fake_useragent
+
+#session, authorization, cookie
+#login: jegorix
+#password: egor12211221
+
+
+session = requests.Session()
+
+# link = "http://forum.watch.ru/login.php?do=login&r=57984"
+# link = "https://automationexercise.com/login"
+# link = "https://practice.expandtesting.com/login"
+# link = "https://the-internet.herokuapp.com/login"
+link = "https://the-internet.herokuapp.com/authenticate"
+profile_link = "https://the-internet.herokuapp.com/secure"
+
+user = fake_useragent.UserAgent().random
+
+header = {
+    'user-agent': user,
+}
+
+
+# data = {
+#     "username": "practice",
+#     "password": "SuperSecretPassword!"
+# }
+
+data = {
+    "username": "tomsmith",
+    "password": "SuperSecretPassword!"
+}
+
+
+response = session.post(link, data=data, headers=header)
+
+print(f"Status code: {response.status_code}")
+print(f"URL: {response.url}")
+
+
+# profile_link = response.url
+def parse_info(profile_link):
+    profile_response = session.get(profile_link, headers=header).text
+    soup = BeautifulSoup(profile_response, "lxml")
+    block = soup.find("div", attrs={"class": "example"})
+    check_area = block.find_all("h4", attrs={"class": "subheader"})
+
+    for h in check_area:
+        print(f"SECURE TEXT: {h.get_text(strip=True)}")
+
+
+
+
+if "Welcome" in response.text or "Logout" in response.text:
+    print("successful login")
+    parse_info(response.url)
+else:
+    print("unsuccessful login")
+
