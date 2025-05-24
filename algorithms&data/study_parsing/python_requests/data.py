cdd58a49cf2a6a5255a1fc9ae50db61394815f33
jegorix <0311egor@gmail.com>
2025-05-24 21:36:24 +0300

    cruicial links and data

--- algorithms&data/study_parsing/python_requests/data.py
+++ algorithms&data/study_parsing/python_requests/data.py
@@ -1,2 +1,10 @@
+import fake_useragent
+
+authorization_url = "https://quotes.toscrape.com/"
+authorization_url_login = "https://quotes.toscrape.com/login"
 API_TOKEN = "60df5caa0b880580fcc637509dbe92ac"
 KELVIN =  273.15
+MAIN_URL = "https://scrapingclub.com"
+IMAGE_PATH = "/Users/macbook/PycharmProjects/scripts_edu/algorithms&data/study_parsing/python_requests/static/images/"
+STATIC_PATH = "/Users/macbook/PycharmProjects/scripts_edu/algorithms&data/study_parsing/python_requests/static/"
+USER_AGENT = fake_useragent.UserAgent().random
