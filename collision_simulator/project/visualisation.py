212f72dde6ae7ed3a90982888b90c043d2535f16
jegorix <0311egor@gmail.com>
2025-05-28 19:18:28 +0300

    comfortable positioning -> make 2 frames for info block

--- collision_simulator/project/visualisation.py
+++ collision_simulator/project/visualisation.py
@@ -2,17 +2,26 @@ import pygame
 import sys
 
 
-def draw_velocity_text(screen, font, body, HEIGHT, name, y_offset=30, x = 0, y = 0):
+def draw_velocity_text(screen, font, body, HEIGHT, name, y_offset=30, x = 0, y = 0, padding = 10):
     lines = [
         f'{name} body:',
         f'Weight: {body.mass}kg',
         f'Speed: {body.velocity // 10:.2f}m/s'
     ]
     
-    for i, line in enumerate(lines):
-        text = font.render(line, True, (0, 0, 0))
-        text_rect = text.get_rect(topleft=(x, y + i * (text.get_height() + 5)))
-        screen.blit(text, text_rect)
+    # comfortable positioning -> make 2 frames for info block
+    rendered_lines = [font.render(line, True, (0,0,0)) for line in lines]
+    line_height = font.get_height()
+    box_width = max(line.get_width() for line in rendered_lines) + padding * 2
+    box_height = len(lines) * line_height + padding * 2 + 5 * (len(lines)-1)
+    
+    background_rect = pygame.Rect(x, y, box_width, box_height)
+    pygame.draw.rect(screen, (200, 200, 200), background_rect, border_radius = 10)
+    pygame.draw.rect(screen, (50, 50, 50), background_rect, 2, border_radius = 10)
+    
+    for i, text in enumerate(rendered_lines):
+        text_pos = (x + padding, y + padding + i * (line_height + 5))
+        screen.blit(text, text_pos)
        
     # text = font.render(text_info, True, (0, 0, 0))
     
@@ -29,7 +38,7 @@ def show_interface(body1, body2):
     WIDTH, HEIGHT = (700, 400)
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     clock = pygame.time.Clock()
-    font = pygame.font.SysFont(None, 24)
+    font = pygame.font.SysFont('consolas', 18, bold=True)
     
     running = True
     while running:
@@ -55,7 +64,7 @@ def show_interface(body1, body2):
         pygame.draw.circle(screen, body2.color, (int(body2.position), HEIGHT // 2), body2.radius)
         
         draw_velocity_text(screen, font, body1, HEIGHT, name='Left', x=20, y=20)
-        draw_velocity_text(screen, font, body2, HEIGHT, name = 'Right', x=WIDTH-150, y=20)
+        draw_velocity_text(screen, font, body2, HEIGHT, name = 'Right', x=WIDTH-205, y=20)
             
         pygame.display.flip()
         
