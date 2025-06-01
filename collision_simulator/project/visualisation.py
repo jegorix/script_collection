6d42edc5eb92dae860fcd40d10e5b0e102f32ed5
jegorix <0311egor@gmail.com>
2025-06-01 15:06:11 +0300

    add all buttons to change velocity

--- collision_simulator/project/visualisation.py
+++ collision_simulator/project/visualisation.py
@@ -22,7 +22,40 @@ pygame.display.set_icon(ico_image)
 object_mars_image = pygame.image.load("/Users/macbook/PycharmProjects/scripts_edu/collision_simulator/image/mars.svg").convert_alpha()
 object_saturno_image = pygame.image.load("/Users/macbook/PycharmProjects/scripts_edu/collision_simulator/image/saturno.svg").convert_alpha()
 
-button_rect = pygame.Rect(30, 156, 80, 30)
+button_increase_obj1 = pygame.Rect(30, 130, 60, 30)
+button_decrease_obj1 = pygame.Rect(110, 130, 60, 30)
+button_increase_obj2 = pygame.Rect(WIDTH-170, 130, 60, 30)
+button_decrease_obj2 = pygame.Rect(WIDTH-90, 130, 60, 30)
+
+buttons = {
+    'increase_1': (button_increase_obj1, "+10m/s"),
+    'decrease_1': (button_decrease_obj1, "-10m/s"),
+    'increase_2': (button_increase_obj2, "+10m/s"),
+    'decrease_2': (button_decrease_obj2, "-10m/s"),
+}
+
+
+def draw_button_velocity(button_rect, name):
+    font = pygame.font.SysFont(FONT_PATH, 25)
+    text_surface = font.render(name, True, (0,0,0))
+    text_rect = text_surface.get_rect(center=button_rect.center)
+    screen.blit(text_surface, text_rect)
+    
+    
+def buttons_operations(body1, body2, mouse_pos):
+    for button_name, button_rect in buttons.items():
+        if button_rect[0].collidepoint(mouse_pos) and button_name == 'increase_1':
+            body1.velocity += 100 if body1.velocity >= 0 else -100
+            
+        elif button_rect[0].collidepoint(mouse_pos) and button_name == 'decrease_1':
+            body1.velocity += -100 if body1.velocity >= 0 else 100
+            
+        elif button_rect[0].collidepoint(mouse_pos) and button_name == 'increase_2':
+            body2.velocity += -100 if body1.velocity <= 0 else 100
+            
+        elif button_rect[0].collidepoint(mouse_pos) and button_name == 'decrease_2':
+            body2.velocity += 100 if body1.velocity <= 0 else -100 
+    
 
 
 
@@ -76,12 +109,9 @@ def show_interface(body1, body2):
             if event.type == pygame.QUIT:
                 running = False
                 # quit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if button_rect.collidepoint(event.pos):
-                   body1.velocity += 100 if body1.velocity >= 0 else -100
-        
-
                 
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                buttons_operations(body1, body2, event.pos) 
                 
                 
         body1.move(dt)
@@ -107,17 +137,14 @@ def show_interface(body1, body2):
         
         
         mouse_pos = pygame.mouse.get_pos()
-        if button_rect.collidepoint(mouse_pos):
-            pygame.draw.rect(screen, DARK_GRAY, button_rect)
-        else:
-            pygame.draw.rect(screen, GRAY, button_rect)
+        for button_rect in buttons.values():
+            if button_rect[0].collidepoint(mouse_pos):
+                pygame.draw.rect(screen, DARK_GRAY, button_rect[0], border_radius=10)
+            else:
+                pygame.draw.rect(screen, GRAY, button_rect[0], border_radius=10)
             
-        font = pygame.font.SysFont(FONT_PATH, 36)
-        text_surface = font.render("+10m/s", True, (255,255,255))
-        text_rect = text_surface.get_rect(center=button_rect.center)
-        screen.blit(text_surface, text_rect)
-        
-        
+        for button in buttons.values():   
+            draw_button_velocity(button[0], name=button[1])   
         
         
         draw_velocity_text(screen, font, body1, name='Left', x=20, y=20)
